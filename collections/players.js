75f7d65a8a0d642028b77186c7207277c9b2d3f3
François St-Germain <fstgerm@gmail.com>
2015-05-18 15:48:35 -0400

    Moved methods from collections

--- collections/players.js
+++ collections/players.js
@@ -1,25 +1,9 @@
-Players = new Meteor.Collection('players');
+Players = new Meteor.Collection('players', {
+    transform: function (doc) {
+        if (Meteor.userId() != doc.userId) {
+            doc.answers = _.map(doc.answers, function(answer) { return answer === 0 ? 0 : 1; });
+        }
 
-Meteor.methods({
-    player: function(gameId) {
-
-        var user    = Meteor.user(),
-            answers = [];
-
-        if (!user) throw new Meteor.Error(401, "You need to login");
-
-        for(var i = 0; i < 81; i++)
-            answers[i] = 0;
-
-        var player = {
-            userId    : user._id,
-            gameId    : gameId,
-            answers   : answers,
-            filled    : [],
-            finished  : false,
-            joinedAt  : new Date().getTime()
-        };
-
-        return Players.insert(player);
+        return doc;
     }
 });
